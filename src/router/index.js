0ddb32c30f6e0c4079a65269b7923ac19c26d581
Georg Braunbeck <georg.braunbeck@impresssol.de>
2021-12-21 15:13:44 +0100

    Lesson - Dynamic Routing
     - static

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import EventList from '@/views/EventList'
 import About from '@/views/About'
+import EventDetails from '@/views/EventDetails'
 
 const routes = [
   {
     path: '/',
     name: 'EventList',
     component: EventList,
+  },
+  {
+    path: '/events/123',
+    name: 'EventDetails',
+    component: EventDetails,
   },
   {
     path: '/about',
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventDetails',
+  data() {
+    return {
+      event: null,
+      id: 123,
+    }
+  },
+
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  },
+}
+</script>
