535b9079b64f8c983e3e72c9b752c180e24060cf
Georg Braunbeck <georg.braunbeck@impresssol.de>
2021-12-21 14:27:16 +0100

    Lesson - API Call with Axios
    - add db.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,23 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import EventList from '@/views/EventList'
+import About from '@/views/About'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'EventList',
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/about',
+    name: 'About',
+    component: About,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
